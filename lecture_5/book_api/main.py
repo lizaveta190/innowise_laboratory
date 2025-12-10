0d1400bbdfe0aebb317705e40f193fe61b903dc3
Lizaveta <li.astapenka@gmail.com>
2025-12-10 17:54:46 +0300

    Implement Book Collection API

--- lecture_5/book_api/main.py
+++ lecture_5/book_api/main.py
@@ -1,15 +1,16 @@
 from typing import Optional
 from sqlalchemy import create_engine, String, func
-from sqlalchemy.orm import DeclarativeBase, Session, Mapped, mapped_column
-from fastapi import FastAPI, HTTPException
+from sqlalchemy.orm import DeclarativeBase, sessionmaker, Session, Mapped, mapped_column
+from fastapi import Depends, FastAPI, HTTPException
 
-# Connect to database
+
 engine = create_engine("sqlite:///books.db")
+SessionLocal = sessionmaker(autoflush=False, bind=engine)
 
 class Base(DeclarativeBase):
     pass
 
-class BookBase(Base):
+class Book(Base):
     __tablename__ = "books"
 
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -19,6 +20,14 @@ class BookBase(Base):
 
 # Create table in database
 Base.metadata.create_all(bind=engine)
+
+# Create a dependency for getting a DB session
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app = FastAPI()
 
@@ -30,80 +39,77 @@ def root():
 
 
 @app.get("/books")
-def get_all_books():
+def get_all_books(db: Session = Depends(get_db)):
     """Get all books."""
 
-    with Session(engine) as session:
-        books = session.query(BookBase).all()
-        result = []
-        for book in books:
-            result.append({
-                "id": book.id,
-                "title": book.title,
-                "author": book.author,
-                "year": book.year
-            })
-        return result
+    books = db.query(Book).all()
+    result = []
+    for book in books:
+        result.append({
+            "id": book.id,
+            "title": book.title,
+            "author": book.author,
+            "year": book.year
+        })
+    return result
 
 
 @app.post("/books")
-def add_book(title: str, author: str, year: Optional[int] = None):
+def add_book(title: str, author: str, year: Optional[int] = None, db: Session = Depends(get_db)):
     """Add a new book."""
 
-    with Session(engine) as session:
-        book = BookBase(title=title, author=author, year=year)
-        session.add(book)
-        session.commit()
-        return {"message": "The book has been added successfully"}
+    book = Book(title=title, author=author, year=year)
+    db.add(book)
+    db.commit()
+    return {"message": "The book has been added successfully"}
 
 
 @app.get("/books/search")
 def find_book(
     title: Optional[str] = None,
     author: Optional[str] = None,
-    year: Optional[int] = None
+    year: Optional[int] = None,
+    db: Session = Depends(get_db)
 ):
     """Search for books by title, author or year."""
 
-    with Session(engine) as session:
-        main_query = session.query(BookBase)
-        
-        if title:
-            main_query = main_query.filter(func.lower(BookBase.title).like(f"%{title.strip().lower()}%"))
-        if author:
-            main_query = main_query.filter(func.lower(BookBase.author).like(f"%{author.strip().lower()}%"))
-        if year:
-            main_query = main_query.filter(BookBase.year == year)
-        
-        books = main_query.all()
+    main_query = db.query(Book)
+    
+    if title:
+        main_query = main_query.filter(func.lower(Book.title).like(f"%{title.strip().lower()}%"))
+    if author:
+        main_query = main_query.filter(func.lower(Book.author).like(f"%{author.strip().lower()}%"))
+    if year:
+        main_query = main_query.filter(Book.year == year)
+    
+    books = main_query.all()
 
-        if not books:
-            return []
-        
-        result = []
-        for book in books:
-            result.append({
-                "id": book.id,
-                "title": book.title, 
-                "author": book.author, 
-                "year": book.year
-            })
-        return result
-        
+    if not books:
+        return []
+    
+    result = []
+    for book in books:
+        result.append({
+            "id": book.id,
+            "title": book.title, 
+            "author": book.author, 
+            "year": book.year
+        })
+    return result
+    
 
 @app.delete("/books/{book_id}")
-def delete_book(book_id: int):
+def delete_book(book_id: int, db: Session = Depends(get_db)):
     """Delete a book by ID."""
 
-    with Session(engine) as session:
-        book = session.query(BookBase).filter(BookBase.id == book_id).first()
+    book = db.query(Book).filter(Book.id == book_id).first()
 
-        if book is None:
-            raise HTTPException(status_code=404, detail="Book not found")
-    
-        session.delete(book)
-        session.commit()
-        return {"message": "The book has been deleted successfully"}    
+    if book is None:
+        raise HTTPException(status_code=404, detail="Book not found")
+
+    db.delete(book)
+    db.commit()
+    return {"message": "The book has been deleted successfully"}    
 
 
 @app.put("/books/{book_id}")
@@ -111,24 +117,23 @@ def update_book(
     book_id: int, 
     title: Optional[str] = None,
     author: Optional[str] = None,
-    year: Optional[int] = None
+    year: Optional[int] = None,
+    db: Session = Depends(get_db)
     ):
     """Update book details."""
 
-    with Session(engine) as session:
-        book = session.query(BookBase).filter(BookBase.id == book_id).first()
+    book = db.query(Book).filter(Book.id == book_id).first()
 
-        if book is None:
-            raise HTTPException(status_code=404, detail="Book not found")
+    if book is None:
+        raise HTTPException(status_code=404, detail="Book not found")
 
-        if title is not None:
-            book.title = title
-        if author is not None:
-            book.author = author
-        if year is not None:
-            book.year = year
-        
-        session.commit()
-        session.refresh(book)
-        return {"message": "The data has been updated successfully"} 
+    if title is not None:
+        book.title = title
+    if author is not None:
+        book.author = author
+    if year is not None:
+        book.year = year
     
+    db.commit()
+    db.refresh(book)
+    return {"message": "The data has been updated successfully"} 
